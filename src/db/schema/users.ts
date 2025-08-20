71f3a3b0bdee9de1ef8397811da7d5baacf9815f
svitorz <vitor.fabio.castro@gmail.com>
2025-08-20 19:25:14 -0300

    feat: create users migration

---
+++ src/db/schema/users.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  created_at: timestamp("created_at"),
+});
