ef50611c1dec7ae3a5f1d69fd376989900ed1193
svitorz <vitor.fabio.castro@gmail.com>
2025-08-20 20:28:40 -0300

    feat: validate req body

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,12 +1,26 @@
 import express from "express";
+import { insertUser } from "../controllers/users";
+
 const router = express.Router();
 
+const userSchema = z.object({
+  name: z.String(),
+  email: z.email(),
+  password: z.String(),
+});
+
 router.get("/users", function (req, res, next) {
+  const parsed = userSchema.safeParse(req.body);
+  if (!parsed.success)
+    return res.status(400).json({ error: parsed.error.flatten() });
+
   res.json({ title: "Express" });
 });
 
-router.post("/users/", function (req, res, next) {
-  res.json("index", { title: "Express" });
+router.post("/users", function (req, res, next) {
+  try {
+    res.json("index", { title: "Express" });
+  } catch (error) {}
 });
 
 router.get("/users/:id", function (req, res, next) {
